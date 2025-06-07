1e7d111ce4d619e41c27796eeae12117567e4722
Oleg Kizyma <139074511+oleg121203@users.noreply.github.com>
2025-06-07 05:30:53 +0300

    CODE-02: Fix Button props and install missing type dependencies

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button as ChakraButton, ButtonProps } from '@chakra-ui/react';
 
-const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+const Button: React.FC<ButtonProps & { loading?: boolean; loadingText?: string }> = ({ children, loading, loadingText, ...props }) => {
   return (
-    <ChakraButton {...props as any}>
+    <ChakraButton {...props as any} isLoading={loading} loadingText={loadingText}>
       {children}
     </ChakraButton>
   );
